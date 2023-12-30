8dd19061875e33ec1e4e5bf2224d4a27b2d4790d
Daniel Bissell <danielabissell@gmail.com>
2023-12-30 10:20:44 -0500

    new query

--- query.py
+++ query.py
@@ -16,11 +16,18 @@ def print_rows_with_separator(event_name, search_string, directory='./saved_dfs'
     # Apply a mask to find rows containing the search string
     matches = df.applymap(lambda x: search_string in str(x)).any(axis=1)
     
+    # ANSI escape code for red color
+    start_red = "\033[91m"
+    end_red = "\033[0m"
+
     # Iterate over each matching row
     for i, row in df[matches].iterrows():
         # Print each column-value pair in the row
         for col, val in row.items():
-            print(f"{col}: {val}")
+            val_str = str(val)
+            # Highlight the search string in red
+            if search_string in val_str:
+                val_str = val_str.replace(search_string, f"{start_red}{search_string}{end_red}")
+            print(f"{col}: {val_str}")
         # Print separator after each row, not after each value
         print('---')
-
